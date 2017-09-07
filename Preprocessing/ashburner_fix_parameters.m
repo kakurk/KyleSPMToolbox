8285df2ac8fce8b87efd427dd1fff02ec919438c
kkurkela <kyleakurkela@gmail.com>
2017-09-07 16:53:06 -0400

    Wrote the ashuburner_fixed_preprocessing paramaters

--- Preprocessing/ashburner_fix_parameters.m
+++ Preprocessing/ashburner_fix_parameters.m
@@ -1,4 +1,4 @@
-function matlabbatch = wildcard_parameters12(runs, subjectid, wildcards, directories)
+function matlabbatch = ashburner_fix_parameters(runs, subjectid, wildcards, directories)
 % wildcard_parameters12     a function for setting SPM preprocessing
 %                           parameters, designed to work with 
 %                           wilcard_preprocess. This is a custom pipeline.
@@ -27,7 +27,7 @@ for crun = 1:length(runs) % for each run entered into this function..
         
     matlabbatch{2}.spm.temporal.st.scans{crun}(1) = cfg_dep(['Realign: Estimate & Reslice: Resliced Images (Sess ' num2str(crun) ')'], substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','sess', '()',{crun}, '.','cfiles'));
 
-    matlabbatch{5}.spm.spatial.normalise.write.subj.resample(crun) = cfg_dep(['Slice Timing: Slice Timing Corr. Images (Sess ' num2str(crun) ')'], substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('()',{crun}, '.','files'));
+    matlabbatch{6}.spm.spatial.normalise.write.subj.resample(crun) = cfg_dep(['Slice Timing: Slice Timing Corr. Images (Sess ' num2str(crun) ')'], substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('()',{crun}, '.','files'));
 
 end
 
@@ -58,83 +58,83 @@ matlabbatch{2}.spm.temporal.st.so       = [1:2:48 2:2:48];
 matlabbatch{2}.spm.temporal.st.refslice = 47;
 matlabbatch{2}.spm.temporal.st.prefix   = 'a';
 
+% Ashburner Fix for Better Normalization
+matlabbatch{3}.spm.util.imcalc.input(1)       = cfg_dep('Realign: Estimate & Reslice: Mean Image', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','rmean'));
+matlabbatch{3}.spm.util.imcalc.output         = 'ashburner_fixed_mean_functional';
+matlabbatch{3}.spm.util.imcalc.outdir         = {''};
+matlabbatch{3}.spm.util.imcalc.expression     = 'i1 + randn(size(i1))*50';
+matlabbatch{3}.spm.util.imcalc.var            = struct('name', {}, 'value', {});
+matlabbatch{3}.spm.util.imcalc.options.dmtx   = 0;
+matlabbatch{3}.spm.util.imcalc.options.mask   = 0;
+matlabbatch{3}.spm.util.imcalc.options.interp = 1;
+matlabbatch{3}.spm.util.imcalc.options.dtype  = 4;
+
 % Run Independent Coregistration Parameters
-matlabbatch{3}.spm.spatial.coreg.estimate.ref(1) = cfg_dep;
-matlabbatch{3}.spm.spatial.coreg.estimate.ref(1).tname = 'Reference Image';
-matlabbatch{3}.spm.spatial.coreg.estimate.ref(1).tgt_spec{1}(1).name  = 'filter';
-matlabbatch{3}.spm.spatial.coreg.estimate.ref(1).tgt_spec{1}(1).value = 'image';
-matlabbatch{3}.spm.spatial.coreg.estimate.ref(1).tgt_spec{1}(2).name  = 'strtype';
-matlabbatch{3}.spm.spatial.coreg.estimate.ref(1).tgt_spec{1}(2).value = 'e';
-matlabbatch{3}.spm.spatial.coreg.estimate.ref(1).sname = 'Realign: Estimate & Reslice: Mean Image';
-matlabbatch{3}.spm.spatial.coreg.estimate.ref(1).src_exbranch = substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1});
-matlabbatch{3}.spm.spatial.coreg.estimate.ref(1).src_output   = substruct('.','rmean');
 
-% Inputting Subject's Anatomical Information
 anat_directory = fullfile(directories.anat, subjectid); % path to this subjects anatomical folder
-matlabbatch{3}.spm.spatial.coreg.estimate.source = {spm_select('ExtFPListRec', anat_directory, wildcards.anat)}; % path to anatomical image
 
-% More Run Independent Coregistartion Parameters
-matlabbatch{3}.spm.spatial.coreg.estimate.ref(1) = cfg_dep('Realign: Estimate & Reslice: Mean Image', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','rmean'));
-matlabbatch{3}.spm.spatial.coreg.estimate.other  = {''};
-matlabbatch{3}.spm.spatial.coreg.estimate.eoptions.cost_fun = 'nmi';
-matlabbatch{3}.spm.spatial.coreg.estimate.eoptions.sep      = [4 2];
-matlabbatch{3}.spm.spatial.coreg.estimate.eoptions.tol      = [0.02 0.02 0.02 0.001 0.001 0.001 0.01 0.01 0.01 0.001 0.001 0.001];
-matlabbatch{3}.spm.spatial.coreg.estimate.eoptions.fwhm     = [7 7];
+matlabbatch{4}.spm.spatial.coreg.estimate.source = {spm_select('ExtFPListRec', anat_directory, wildcards.anat)}; % path to anatomical image
+matlabbatch{4}.spm.spatial.coreg.estimate.ref(1) = cfg_dep('Image Calculator: ImCalc Computed Image: output', substruct('.','val', '{}',{3}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','files'));
+matlabbatch{4}.spm.spatial.coreg.estimate.other  = {''};
+matlabbatch{4}.spm.spatial.coreg.estimate.eoptions.cost_fun = 'nmi';
+matlabbatch{4}.spm.spatial.coreg.estimate.eoptions.sep      = [4 2];
+matlabbatch{4}.spm.spatial.coreg.estimate.eoptions.tol      = [0.02 0.02 0.02 0.001 0.001 0.001 0.01 0.01 0.01 0.001 0.001 0.001];
+matlabbatch{4}.spm.spatial.coreg.estimate.eoptions.fwhm     = [7 7];
 
 % Run Indepednet Segmentation Parameters
 
 spm_segment_image = which('TPM.nii'); % find the TPM image on the MATLAB search path
 
-matlabbatch{4}.spm.spatial.preproc.channel.vols(1)  = cfg_dep('Coregister: Estimate: Coregistered Images', substruct('.','val', '{}',{3}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','cfiles'));
-matlabbatch{4}.spm.spatial.preproc.channel.biasreg  = 0.001;
-matlabbatch{4}.spm.spatial.preproc.channel.biasfwhm = 60;
-matlabbatch{4}.spm.spatial.preproc.channel.write    = [0 0];
-matlabbatch{4}.spm.spatial.preproc.tissue(1).tpm    = {[spm_segment_image ',1']};
-matlabbatch{4}.spm.spatial.preproc.tissue(1).ngaus  = 1;
-matlabbatch{4}.spm.spatial.preproc.tissue(1).native = [1 0];
-matlabbatch{4}.spm.spatial.preproc.tissue(1).warped = [0 0];
-matlabbatch{4}.spm.spatial.preproc.tissue(2).tpm    = {[spm_segment_image ',2']};
-matlabbatch{4}.spm.spatial.preproc.tissue(2).ngaus  = 1;
-matlabbatch{4}.spm.spatial.preproc.tissue(2).native = [1 0];
-matlabbatch{4}.spm.spatial.preproc.tissue(2).warped = [0 0];
-matlabbatch{4}.spm.spatial.preproc.tissue(3).tpm    = {[spm_segment_image ',3']};
-matlabbatch{4}.spm.spatial.preproc.tissue(3).ngaus  = 2;
-matlabbatch{4}.spm.spatial.preproc.tissue(3).native = [1 0];
-matlabbatch{4}.spm.spatial.preproc.tissue(3).warped = [0 0];
-matlabbatch{4}.spm.spatial.preproc.tissue(4).tpm    = {[spm_segment_image ',4']};
-matlabbatch{4}.spm.spatial.preproc.tissue(4).ngaus  = 3;
-matlabbatch{4}.spm.spatial.preproc.tissue(4).native = [1 0];
-matlabbatch{4}.spm.spatial.preproc.tissue(4).warped = [0 0];
-matlabbatch{4}.spm.spatial.preproc.tissue(5).tpm    = {[spm_segment_image ',5']};
-matlabbatch{4}.spm.spatial.preproc.tissue(5).ngaus  = 4;
-matlabbatch{4}.spm.spatial.preproc.tissue(5).native = [1 0];
-matlabbatch{4}.spm.spatial.preproc.tissue(5).warped = [0 0];
-matlabbatch{4}.spm.spatial.preproc.tissue(6).tpm    = {[spm_segment_image ',6']};
-matlabbatch{4}.spm.spatial.preproc.tissue(6).ngaus  = 2;
-matlabbatch{4}.spm.spatial.preproc.tissue(6).native = [0 0];
-matlabbatch{4}.spm.spatial.preproc.tissue(6).warped = [0 0];
-matlabbatch{4}.spm.spatial.preproc.warp.mrf         = 1;
-matlabbatch{4}.spm.spatial.preproc.warp.cleanup     = 1;
-matlabbatch{4}.spm.spatial.preproc.warp.reg         = [0 0.001 0.5 0.05 0.2];
-matlabbatch{4}.spm.spatial.preproc.warp.affreg      = 'mni';
-matlabbatch{4}.spm.spatial.preproc.warp.fwhm        = 0;
-matlabbatch{4}.spm.spatial.preproc.warp.samp        = 3;
-matlabbatch{4}.spm.spatial.preproc.warp.write       = [0 1];
+matlabbatch{5}.spm.spatial.preproc.channel.vols(1)  = cfg_dep('Coregister: Estimate: Coregistered Images', substruct('.','val', '{}',{3}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','cfiles'));
+matlabbatch{5}.spm.spatial.preproc.channel.biasreg  = 0.001;
+matlabbatch{5}.spm.spatial.preproc.channel.biasfwhm = 60;
+matlabbatch{5}.spm.spatial.preproc.channel.write    = [0 0];
+matlabbatch{5}.spm.spatial.preproc.tissue(1).tpm    = {[spm_segment_image ',1']};
+matlabbatch{5}.spm.spatial.preproc.tissue(1).ngaus  = 1;
+matlabbatch{5}.spm.spatial.preproc.tissue(1).native = [1 0];
+matlabbatch{5}.spm.spatial.preproc.tissue(1).warped = [0 0];
+matlabbatch{5}.spm.spatial.preproc.tissue(2).tpm    = {[spm_segment_image ',2']};
+matlabbatch{5}.spm.spatial.preproc.tissue(2).ngaus  = 1;
+matlabbatch{5}.spm.spatial.preproc.tissue(2).native = [1 0];
+matlabbatch{5}.spm.spatial.preproc.tissue(2).warped = [0 0];
+matlabbatch{5}.spm.spatial.preproc.tissue(3).tpm    = {[spm_segment_image ',3']};
+matlabbatch{5}.spm.spatial.preproc.tissue(3).ngaus  = 2;
+matlabbatch{5}.spm.spatial.preproc.tissue(3).native = [1 0];
+matlabbatch{5}.spm.spatial.preproc.tissue(3).warped = [0 0];
+matlabbatch{5}.spm.spatial.preproc.tissue(4).tpm    = {[spm_segment_image ',4']};
+matlabbatch{5}.spm.spatial.preproc.tissue(4).ngaus  = 3;
+matlabbatch{5}.spm.spatial.preproc.tissue(4).native = [1 0];
+matlabbatch{5}.spm.spatial.preproc.tissue(4).warped = [0 0];
+matlabbatch{5}.spm.spatial.preproc.tissue(5).tpm    = {[spm_segment_image ',5']};
+matlabbatch{5}.spm.spatial.preproc.tissue(5).ngaus  = 4;
+matlabbatch{5}.spm.spatial.preproc.tissue(5).native = [1 0];
+matlabbatch{5}.spm.spatial.preproc.tissue(5).warped = [0 0];
+matlabbatch{5}.spm.spatial.preproc.tissue(6).tpm    = {[spm_segment_image ',6']};
+matlabbatch{5}.spm.spatial.preproc.tissue(6).ngaus  = 2;
+matlabbatch{5}.spm.spatial.preproc.tissue(6).native = [0 0];
+matlabbatch{5}.spm.spatial.preproc.tissue(6).warped = [0 0];
+matlabbatch{5}.spm.spatial.preproc.warp.mrf         = 1;
+matlabbatch{5}.spm.spatial.preproc.warp.cleanup     = 1;
+matlabbatch{5}.spm.spatial.preproc.warp.reg         = [0 0.001 0.5 0.05 0.2];
+matlabbatch{5}.spm.spatial.preproc.warp.affreg      = 'mni';
+matlabbatch{5}.spm.spatial.preproc.warp.fwhm        = 0;
+matlabbatch{5}.spm.spatial.preproc.warp.samp        = 3;
+matlabbatch{5}.spm.spatial.preproc.warp.write       = [0 1];
 
 
 % Run Indepenedent Normalization: Normalizaing the Functional Images
-matlabbatch{5}.spm.spatial.normalise.write.subj.def(1) = cfg_dep('Segment: Forward Deformations', substruct('.','val', '{}',{4}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','fordef', '()',{':'}));
-matlabbatch{5}.spm.spatial.normalise.write.woptions.bb = [-78 -112 -70
+matlabbatch{6}.spm.spatial.normalise.write.subj.def(1) = cfg_dep('Segment: Forward Deformations', substruct('.','val', '{}',{5}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','fordef', '()',{':'}));
+matlabbatch{6}.spm.spatial.normalise.write.woptions.bb = [-78 -112 -70
                                                           78 76 85];
-matlabbatch{5}.spm.spatial.normalise.write.woptions.vox    = [2 2 2];
-matlabbatch{5}.spm.spatial.normalise.write.woptions.interp = 4;
-matlabbatch{5}.spm.spatial.normalise.write.woptions.prefix = 'w';
+matlabbatch{6}.spm.spatial.normalise.write.woptions.vox    = [2 2 2];
+matlabbatch{6}.spm.spatial.normalise.write.woptions.interp = 4;
+matlabbatch{6}.spm.spatial.normalise.write.woptions.prefix = 'w';
 
 % Run Indepednent Smoothing Parameters
-matlabbatch{6}.spm.spatial.smooth.data(1) = cfg_dep('Normalise: Write: Normalised Images (Subj 1)', substruct('.','val', '{}',{5}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('()',{1}, '.','files'));
-matlabbatch{6}.spm.spatial.smooth.fwhm    = [6 6 6];
-matlabbatch{6}.spm.spatial.smooth.dtype   = 0;
-matlabbatch{6}.spm.spatial.smooth.im      = 0;
-matlabbatch{6}.spm.spatial.smooth.prefix  = 's';
+matlabbatch{7}.spm.spatial.smooth.data(1) = cfg_dep('Normalise: Write: Normalised Images (Subj 1)', substruct('.','val', '{}',{6}, '.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('()',{1}, '.','files'));
+matlabbatch{7}.spm.spatial.smooth.fwhm    = [6 6 6];
+matlabbatch{7}.spm.spatial.smooth.dtype   = 0;
+matlabbatch{7}.spm.spatial.smooth.im      = 0;
+matlabbatch{7}.spm.spatial.smooth.prefix  = 's';
 
 end
